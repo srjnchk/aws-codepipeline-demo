920fad12d491f798e189f2d625aba9abadf9d748
srjnchk <srijan.chakraborty@accenture.com>
2020-02-17 16:51:05 -0500

    v2 change

--- src/components/Root/Counter/Counter.test.js
+++ src/components/Root/Counter/Counter.test.js
@@ -7,7 +7,7 @@ test("Counter component works properly", () => {
   const { getByTestId, getByText } = render(<Counter />);
 
   const counterValueEl = getByTestId("counter-value");
-  const minusTwoButtonEl = getByText("sample", { selector: "button" });
+  const minusTwoButtonEl = getByText("-9", { selector: "button" });
   const minusOneButtonEl = getByText("-1", { selector: "button" });
   const plusOneButtonEl = getByText("+1", { selector: "button" });
   const plusTwoButtonEl = getByText("+2", { selector: "button" });
